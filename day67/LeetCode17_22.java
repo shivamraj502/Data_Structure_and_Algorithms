7181e3eb7e46a232faf4d63ebb9fb378c84723f6
shivam_raj <shivamraj81020051@gmail.com>
2026-05-19 01:48:52 +0530

    leetcode 22: list all valid parenthesis

---
+++ day67/LeetCode17_22.java
@@ -0,0 +1,42 @@
+/**
+Day 67 – Recursion Practice
+Problems to Solve:
+Letter Combinations of Phone Number – LeetCode 17
+Generate Parentheses – LeetCode22
+Goal: Practice tree-based recursion.
+ */
+
+public class LeetCode17_22 {
+    public static List<List<String>> leet17(int [] nums){
+
+        return resultList;
+    }
+    public static List<List<String>> leet22(int [] nums){
+
+        return resultList;
+    }
+    public static void main(String[] args) {
+        int [] nums = {1,2,3};
+        System.out.println(leet17(nums));
+        // System.out.println(leet22(nums));
+    }
+}
+
+
+/*
+17. Letter Combinations of a Phone Number
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+Input: digits = "2"
+Output: ["a","b","c"]
+
+22. Generate Parentheses
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+Input: n = 1
+Output: ["()"]
+ */
