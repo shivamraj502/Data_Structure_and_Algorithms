0276e772a447392288eb342f02296c4fbbaa62e2
shivam_raj <shivamraj81020051@gmail.com>
2026-06-15 22:21:16 +0530

    print n numbers from 1

--- day69/LeetCode51.java
+++ day69/LeetCode51.java
@@ -5,7 +5,27 @@ Problem: N-Queens – LeetCode 51
 Goal: Learn how to "backtrack" safely.
  */
 public class LeetCode51 {
+    public static void printN(int n){
+        for(int i=01;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+    public static void printN2(int n){
+       if(n==0) return ;
+       printN(n-1);
+       System.out.print(n +" ");
+    }
+    public static void printBinN1(int n){
+       if(n==0) return ;
+       printN(n-1);
+       System.out.print(n +" ");
+    }
     public static void main(String[] args) {
-        
+        printBinN1(3);
     }
 }
+
+/**
+ * 1️⃣ Print Numbers from 1 to N
+ * 2️⃣ Print All Binary Strings of Length N
+ */
