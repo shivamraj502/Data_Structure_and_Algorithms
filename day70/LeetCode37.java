24ecdfe07d6fe964e27e87ee061145fdcf2abb95
shivam_raj <shivamraj81020051@gmail.com>
2026-06-16 20:21:38 +0530

    leetcode 37: suduko

---
+++ day70/LeetCode37.java
@@ -0,0 +1,11 @@
+/**
+Day 70 – Sudoku Solver
+Concept: Recursive filling with constraints.
+Problem: Sudoku Solver – LeetCode 37
+Goal: Learn constraint satisfaction via backtracking.
+ */
+public class LeetCode37 {
+    public static void main(String[] args) {
+        
+    }
+}
