33a9b3c332aff15271c0a814b09d50397f5b09ee
shivam_raj <shivamraj81020051@gmail.com>
2026-06-16 19:39:29 +0530

    ran and maze

--- day69/LeetCode51.java
+++ day69/LeetCode51.java
@@ -128,10 +128,40 @@ public class LeetCode51 {
       }
     }
     
+    public static boolean ratNmaze(int [][] arr, int i, int j){       
+      if(i == arr.length-1 && j == arr[0].length-1){
+         return true;
+      }
+      
+      if(i >= arr.length || j >= arr[0].length){
+         return false;
+      }
+
+      if(arr[i][j] == 0){
+         return false;
+      }
+
+      arr[i][j] = 0;
+
+      if(ratNmaze(arr,i+1,j)){
+         return true;
+      }
+      
+      if(ratNmaze(arr,i,j+1)){
+         return true;
+      }
+
+      arr[i][j]=1;
+
+       return false;
+    }
+
 
     public static void main(String[] args) {
-        String digits = "23";
-        System.out.println(LetComb(digits));
+        int[][] arr = {{1,0,0},
+                        {1,0,0},
+                        {0,0,0}};
+        System.out.println(ratNmaze(arr,0,0));
     }
 }
 
