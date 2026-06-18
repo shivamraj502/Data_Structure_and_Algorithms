722af11eb57b087f049ba7124134ab489a4341d5
shivam_raj <shivamraj81020051@gmail.com>
2026-06-18 12:53:49 +0530

    sum of two inputs number

--- day1/SumOfTwoNumber.java
+++ day1/SumOfTwoNumber.java
@@ -1,8 +1,11 @@
-//  SumOfTwoNumber 
+//  SumOfTwoNumber: Take Input and Print Sum of Two Numbers – GFG
 
 import java.util.Scanner;
 class SumOfTwoNumber{
-    public static void main(String[] args) {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("enter a and b:");
@@ -13,4 +16,12 @@ class SumOfTwoNumber{
             System.out.println("sum is:"+(a+b));
            
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(sum(a,b));
+    }
+
 }
