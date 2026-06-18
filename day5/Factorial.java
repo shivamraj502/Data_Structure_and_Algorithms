f1fbcee1dc112675f3c9ddd4f3efac399292e660
shivam_raj <shivamraj81020051@gmail.com>
2026-06-18 17:40:32 +0530

    calculate factorial

--- day5/Factorial.java
+++ day5/Factorial.java
@@ -6,21 +6,30 @@
 import java.util.Scanner;
 
 public class Factorial {
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("enter a number for factorial:");
         int n = in.nextInt();
 
-        int result = fact(n);
+        int result = fact2(n);
         System.out.println("factorial of " + n + " is " + result);
     }    
 
-    public static int fact(int n){
+    public static int fact2(int n){
         int product =1;
         for(int i=1;i<=n;i++){
             product *=i;
         }
         return product;
+    }
+
+    public static int fact(int n){
+        if(n <= 0) return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fact(n));
     }
 }
