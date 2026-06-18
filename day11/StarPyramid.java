0abebcc7f99ed2d770ace8e99614aa56ed99f7bb
shivam_raj <shivamraj81020051@gmail.com>
2026-06-18 21:50:55 +0530

    Star Pattern

--- day11/StarPyramid.java
+++ day11/StarPyramid.java
@@ -5,7 +5,22 @@
 
 import java.util.Scanner;
 public class StarPyramid {
-    public static void main(String[] args) {
+   public static void pyr(int n){
+      for(int i=0;i<n;i++){
+         for(int j=0;j<2-i;j++){
+            System.out.print("   ");
+         }
+         for(int k=0;k<2*i+1;k++){
+            System.out.print(" * ");
+         }System.out.println();
+      }
+   }
+   public static void main(String [] args){
+      int n= 3;
+      pyr(n);
+   }
+
+    public static void main2(String[] args) {
     Scanner in = new Scanner(System.in);
     int n;
     System.out.println("enter level of pyramid: ");
