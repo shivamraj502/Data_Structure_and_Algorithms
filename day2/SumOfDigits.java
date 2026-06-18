c71574cd7dc1d0cc7e223dbd6706a07c1bd0fd1e
shivam_raj <shivamraj81020051@gmail.com>
2026-06-18 17:20:27 +0530

    sum of digits

--- day2/SumOfDigits.java
+++ day2/SumOfDigits.java
@@ -1,24 +1,41 @@
 // Sum of Digits of a Number
 
-import java.util.Scanner;
+// import java.util.Scanner;
 public class SumOfDigits {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n ;
-        int sum=0;
-        int rem;
+    public static int sum(int n){
+        int s = 0;
 
-        System.out.println("enter a number: ");
-        n = in.nextInt();
+        while(n>0){
+        int rem = n % 10;
+        s += rem;
+        n /= 10;
+        }
+        return s;
+    }
 
-        if(n>=0){
-        do{
-            rem = n % 10;
-            sum += rem;
-            n /=10;
-        }while(n>0);
-        System.out.print("sum is: "+sum);
-    }else System.out.println("invalid number");
+    // public static void main2(String[] args) {
+    //     Scanner in = new Scanner(System.in);
+    //     int n ;
+    //     int sum=0;
+    //     int rem;
 
+    //     System.out.println("enter a number: ");
+    //     n = in.nextInt();
+
+    //     if(n>=0){
+    //     do{
+    //         rem = n % 10;
+    //         sum += rem;
+    //         n /=10;
+    //     }while(n>0);
+    //     System.out.print("sum is: "+sum);
+    // }else System.out.println("invalid number");
+
+    // }
+
+    public static void main(String [] args){
+        int n = 104501;
+        System.out.println(sum(n));
+        
     }
 }
