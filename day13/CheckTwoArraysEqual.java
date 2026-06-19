acf1c38a52352e3c82c4acb3a0c029aac5c5fbdd
shivam_raj <shivamraj81020051@gmail.com>
2026-06-19 12:46:12 +0530

    Compare two arrays

--- day13/CheckTwoArraysEqual.java
+++ day13/CheckTwoArraysEqual.java
@@ -3,10 +3,22 @@
 // Concept: Enhanced for, Arrays.sort(), Arrays.equals().
 // Problem: Check Two Arrays Equal – GFG
 // Goal: Use built-in utilities.
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CheckTwoArraysEqual {
+    public static boolean isEq(int [] arr1, int [] arr2){
+        // return arr1.equals(arr2);
+        return Arrays.equals(arr1,arr2);
+    }
     public static void main(String[] args) {
+        int [] arr1 = {1,2,3,3};
+        int [] arr2 = {1,2,3};
+        System.out.println(isEq(arr1, arr2));
+    }
+
+
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("enter array1 size(n):");
@@ -43,3 +55,14 @@ public class CheckTwoArraysEqual {
         }
     }
 }
+
+            //Array comparison mistake in Java
+//arr1.equals(arr2) compares memory addresses, not values
+//Arrays are objects in Java
+//Same values + different memory = false
+//Example:[1,2,3] and [1,2,3] → false
+//equals() checks reference equality for arrays
+//Use Arrays.equals(arr1,arr2) to compare values
+//Arrays.equals() checks length + each element
+//arr1.equals(arr2) 
+//Arrays.equals(arr1,arr2) 
