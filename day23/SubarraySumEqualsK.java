3f65613da0219214e0511e23fe512bb4788231dd
shivam_raj <shivamraj81020051@gmail.com>
2026-06-21 11:26:29 +0530

    leetcode 560: Subarray sum equal k

--- day23/SubarraySumEqualsK.java
+++ day23/SubarraySumEqualsK.java
@@ -6,7 +6,24 @@ Problem: Subarray Sum Equals K – LeetCode 560
 
 import java.util.*;
 public class SubarraySumEqualsK {
+    public static int subArr(int [] arr, int k){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            int sum = 0;
+            for(int j=i;j<arr.length;j++){
+                sum += arr[j];
+                if(sum == k){ count++;}
+            }
+        }return count;
+    }
     public static void main(String[] args) {
+        int [] arr = {1,1,1};
+        int k = 2;
+        System.out.println(subArr(arr, k));
+    }
+
+
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
         int sum = 0;
         int count =0;
