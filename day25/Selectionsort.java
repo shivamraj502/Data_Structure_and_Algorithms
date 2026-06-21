b76b66b3b1426643d487858f95c951ca0e3ee7af
shivam_raj <shivamraj81020051@gmail.com>
2026-06-21 11:59:04 +0530

    Selection sort

--- day25/Selectionsort.java
+++ day25/Selectionsort.java
@@ -2,7 +2,29 @@
 
 import java.util.Scanner;
 class Selectionsort {
+    public static void sel(int [] arr){
+        
+        for(int i=0;i<arr.length-1;i++){
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){min = j;}
+            }
+            int temp = arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+    }
     public static void main(String[] args) {
+        int arr[] = {1,3,2,43,5,55,4};
+        sel(arr);
+    }
+
+
+
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter array length: ");
         int n = in.nextInt();
