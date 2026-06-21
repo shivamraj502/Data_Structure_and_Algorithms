37f347245ed9c0f0adc94e09f085f7bba86d3af1
shivam_raj <shivamraj81020051@gmail.com>
2026-06-21 11:35:16 +0530

    Reverse array using 2 pointers

--- day24/ReverseArray.java
+++ day24/ReverseArray.java
@@ -6,7 +6,28 @@ Problem: Reverse Array – GFG
 
 import java.util.*;
 public class ReverseArray {
+    public static void rev(int [] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+    }
     public static void main(String[] args) {
+        int [] arr = {1,2,3,4};
+        rev(arr);
+    }
+
+
+    public static void main2(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter array length: ");
