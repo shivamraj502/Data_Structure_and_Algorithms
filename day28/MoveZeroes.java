b88a894ba0174d669a1c3c7cad387db2879d29c5
shivam_raj <shivamraj81020051@gmail.com>
2026-06-22 13:16:43 +0530

    Leetcode283: remove zerox

--- day28/MoveZeroes.java
+++ day28/MoveZeroes.java
@@ -15,7 +15,34 @@ Result:[1, 3, 12, 0, 0]
  */
 
 public class MoveZeroes {
+    public static int [] rem(int [] nums){
+        int count =0;
+
+        for(int i=0;i<nums.length-count;i++){
+            if(nums[i] == 0){
+
+                for(int j=i;j<nums.length-1-count;j++){
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+                }count++;
+                // i--;
+                if(i<nums.length-1 && nums[i] == 0 ) i--;
+            }
+        }
+        return nums;
+    }
     public static void main(String[] args) {
+        int [] arr = {1, 0, 0, 3, 12};
+        int [] res = rem(arr);
+
+        for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+" ");
+        }System.out.println();
+    }
+
+
+    public static void main2(String[] args) {
         int [] arr = {0, 0, 0, 0, 12};
         int n = arr.length;
 
