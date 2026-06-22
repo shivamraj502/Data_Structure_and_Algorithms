8fdb7dfa50c05af2f5c787e0353253f28689b162
shivam_raj <shivamraj81020051@gmail.com>
2026-06-22 12:02:05 +0530

    Reverse words

--- day27/ReverseWords.java
+++ day27/ReverseWords.java
@@ -3,7 +3,32 @@
 // Problem: ReverseWords in a String III – LeetCode 557
 
 public class ReverseWords {
+    public static String str3(String s){
+        StringBuilder str = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                str.append(temp.reverse());
+                str.append(" ");
+                // temp = new StringBuilder(""); 
+                temp.setLength(0);  // temp.setLength(0);                  //reset,empty
+            }else{ temp.append(s.charAt(i));}
+        }str.append(temp.reverse());
+        
+        String s2 = new String();
+        for(int i=0; i<str.length();i++){
+            s2 += str.charAt(i);
+        }
+        
+        return s2;
+    }
     public static void main(String[] args) {
+        String s = "hi hello";
+        System.out.println(str3(s));
+    }
+
+
+    public static void main2(String[] args) {
         StringBuilder s = new StringBuilder("hello i am shivam");
 
         StringBuilder result = new StringBuilder();
