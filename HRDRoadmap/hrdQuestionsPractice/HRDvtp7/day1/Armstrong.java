9657126890f041f8752fe24d217422c930ae0f9a
shivam_raj <shivamraj81020051@gmail.com>
2026-06-29 14:48:24 +0530

    Armstrong for range

--- HRDRoadmap/hrdQuestionsPractice/HRDvtp7/day1/Armstrong.java
+++ HRDRoadmap/hrdQuestionsPractice/HRDvtp7/day1/Armstrong.java
@@ -1,11 +1,20 @@
 public class Armstrong {
-    public static boolean isArm(int n){
-        int m = n;
-        int count =0;
-        while(m>0){
-             m = m /10;
-             count++;
-        }
+    public static int isArm(int n,int z){
+        int total = 0;
+
+        for(int k=n;k<z;k++){
+        int count =(""+n).length();
+        
+        // int m = n;
+        // while(m>0){
+        //      m = m /10;
+        //      count++;
+        // }
+
+        //int length = (""+n).length();
+        //int length = (int)(Math.log10(n)+1);
+        // int length = Integer.toString(n).length();
+        // System.out.println("length: "+length);
 
         int p = n;
         int sum = 0;
@@ -17,13 +26,15 @@ public class Armstrong {
             for(int j=0;j<count;j++){
                 product *= rem;
             }sum += product;
-        }
+            
+        }if(sum == n){ total++;}
         
-        return sum==n;
+        }return total;
     }
 
     public static void main(String[] args) {
-        int a =154;
-        System.out.println(isArm(a));
+        int a =370; //370,371,153
+        int b =372;
+        System.out.println(isArm(a,b)+"\n");
     }
 }
