feed587414d5a7c7910b928f79ef4ca2937ad98c
shivam_raj <shivamraj81020051@gmail.com>
2026-06-29 15:17:45 +0530

    sum of range of numbers

---
+++ HRDRoadmap/hrdQuestionsPractice/HRDvtp7/day1/sumOfAllNum.java
@@ -0,0 +1,18 @@
+public class sumOfAllNum {
+    public static void Sum(int a, int b){
+        System.out.println((b*(b+1)/2) -(a*(a+1)/2));
+        
+        //int sum=0;
+        // for(int i=a;i<=b;i++){
+        //     sum +=i;
+        // }return sum;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7777;
+        Sum(a, b);
+        // System.out.println(Sum(a, b));
+    }
+}
