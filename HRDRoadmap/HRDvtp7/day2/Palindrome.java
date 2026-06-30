91058ed30f264a5267e44d85af6fc05b739111a3
shivam_raj <shivamraj81020051@gmail.com>
2026-06-30 12:05:06 +0530

    check Palindrome

--- HRDRoadmap/HRDvtp7/day2/Palindrome.java
+++ HRDRoadmap/HRDvtp7/day2/Palindrome.java
@@ -1,9 +1,9 @@
 public class Palindrome {
     public static boolean isPal(String s){
+        s = s.toLowerCase().trim();
         boolean bool = true;
         int left = 0;
         int right = s.length()-1;
-        s = s.toLowerCase();
         
         while(left < right){
             if(s.charAt(left) != s.charAt(right)){return !bool;}
@@ -13,7 +13,7 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-        String s = "madaM";
+        String s = "madaM   ";
         System.out.println(isPal(s));
     }
 }
