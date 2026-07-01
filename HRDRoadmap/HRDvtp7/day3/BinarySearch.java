8fdd64c6f5fdc82afad2ae9252c7a41dc20cdd9e
shivam_raj <shivamraj81020051@gmail.com>
2026-07-01 12:20:50 +0530

    binary search

---
+++ HRDRoadmap/HRDvtp7/day3/BinarySearch.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static int bs(int []arr,int t){
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low <= high){         // "=" to search last and first element also
+            int mid = low + (high-low)/2;
+            if(arr[mid]== t){
+                return mid;
+            }else if(arr[mid]>t){   //Mistake: i written mid > low and low = mid + 1
+                high = mid - 1;
+            }else low = mid + 1;
+        }return -1;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,5,44};
+        int t = 44;
+        System.out.println(bs(arr, t));
+    }
+}
+
+
+// Q: why O(n) = log(n) base(2)
