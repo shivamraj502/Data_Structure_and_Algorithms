e5ee1b89dec9b3d4a9f83c09ab718a93af62482a
shivam_raj <shivamraj81020051@gmail.com>
2026-07-01 22:50:49 +0530

    leetcode 33

--- HRDRoadmap/HRDvtp7/day3/Leetcode33.java
+++ HRDRoadmap/HRDvtp7/day3/Leetcode33.java
@@ -1,7 +1,13 @@
 public class Leetcode33 {
-
+    public static int search(int [] nums,int target){
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){ return i;}
+        }return -1;
+    }
     public static void main(String[] args) {
-        
+        int [] arr = {4,5,0,1,2,3};
+        int target = 0;
+        System.out.println(search(arr, target));
         
     }
 }
