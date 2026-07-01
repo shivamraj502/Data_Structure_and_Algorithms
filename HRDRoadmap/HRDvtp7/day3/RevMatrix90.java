a179b97ede3d199cfa38f49739f74db79ccd225f
shivam_raj <shivamraj81020051@gmail.com>
2026-07-01 10:34:35 +0530

    rotate 90 degree to matrix

---
+++ HRDRoadmap/HRDvtp7/day3/RevMatrix90.java
@@ -0,0 +1,34 @@
+public class RevMatrix90 {
+    public static int[][] revMatrix(int[][] arr1){
+        int [][] res = new int[arr1.length][arr1.length];
+        
+        for(int i=0;i<arr1.length;i++){
+                for(int j=0;j<arr1.length;j++){
+                        res[i][j] = arr1[j][i];
+                }
+        }
+        
+        for(int i=0;i<arr1.length;i++){
+                for(int j=0;j<arr1.length/2;j++){
+                        int temp2 = res[i][j] ;
+                        res[i][j] = res[i][arr1[0].length-j-1];
+                        res [i][arr1[0].length-j-1] = temp2;
+                }
+        }
+
+        for(int i=0;i<arr1.length;i++){
+                for(int j=0;j<arr1.length;j++){
+                    System.out.print(res[i][j]+" ") ;
+                }System.out.println();
+        }return res;
+    }
+    public static void main(String[] args) {    // can be done it using reversal of each one-D array of Result. for(int[] row: matrix){Array.reverse(row)}
+        int [][] arr1 = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        // int [][] res = transpose(arr1);
+        revMatrix(arr1);
+    }
+}
