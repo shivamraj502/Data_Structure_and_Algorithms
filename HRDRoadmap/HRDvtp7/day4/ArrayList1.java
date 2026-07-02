7baa8da65337eb97cafd58458f19f114e42ba8cf
shivam_raj <shivamraj81020051@gmail.com>
2026-07-02 12:31:28 +0530

    collections: ArrayList

--- HRDRoadmap/HRDvtp7/day4/ArrayList1.java
+++ HRDRoadmap/HRDvtp7/day4/ArrayList1.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 public class ArrayList1 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         list.add(8);
         list.add(248);
