b4f1fde39a3fa256ed1967a24e295747b8736223
shivam_raj <shivamraj81020051@gmail.com>
2026-07-02 12:30:42 +0530

    collections: priorityQueue

--- HRDRoadmap/HRDvtp7/day4/PriorityQueue1.java
+++ HRDRoadmap/HRDvtp7/day4/PriorityQueue1.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+// import java.util.*;
+
 public class PriorityQueue1 {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+
         pq.add(1);
         pq.add(222);
         pq.add(3);
