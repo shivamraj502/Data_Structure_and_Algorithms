e1729db041e8adbd8506332e04acfc342c36730b
shivam_raj <shivamraj81020051@gmail.com>
2026-07-02 12:07:03 +0530

    collections: hashset

---
+++ HRDRoadmap/HRDvtp7/day4/HashSet1.java
@@ -0,0 +1,13 @@
+// package HRDRoadmap.HRDvtp7.day4;
+import java.util.*;
+public class HashSet1 {     // Mistakes: i written class name and function same which is not possible for generic data type
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        set.add(3);
+        set.add(5);
+
+        System.out.println(set);
+    }
+}
