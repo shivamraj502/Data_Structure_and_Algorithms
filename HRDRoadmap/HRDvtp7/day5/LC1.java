f4b7d23c36738fa535bea835f22b5497efaf2921
shivam_raj <shivamraj81020051@gmail.com>
2026-07-03 11:02:44 +0530

    leetcode: 1 and 268

---
+++ HRDRoadmap/HRDvtp7/day5/LC1.java
@@ -0,0 +1,41 @@
+// package HRDRoadmap.HRDvtp7.day5;
+import java.util.Arrays;
+public class LC1 {
+    public static int [] arr(int [] nums,int t){
+        int [] res = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==t){
+                    res[0]=i;res[1]=j;
+                }
+            }
+        }return res;
+    }
+    public static int [] arr2(int [] nums,int target){
+        int [] res = new int[2];
+        int sum = nums[0]+nums[1];
+
+        
+        return res;
+    }
+    public static void main(String[] args) {
+        // int [] nums = {2,7,11,15};
+        // int t = 9;
+        int [] nums = {3,2,4};
+        int t = 6;
+        int [] result = arr(nums, t);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
---
+++ HRDRoadmap/HRDvtp7/day5/LL1.java
@@ -0,0 +1,19 @@
+// package HRDRoadmap.HRDvtp7.day5;
+public class LL1 {
+    public class Node{
+        int data;
+        Node next;
+        Node(int data,Node next){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void display(){
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
