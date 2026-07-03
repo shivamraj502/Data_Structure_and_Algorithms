12503dfda42b31838668def8e815bd6aec6dbece
shivam_raj <shivamraj81020051@gmail.com>
2026-07-03 08:46:45 +0530

    stack

--- HRDRoadmap/HRDvtp7/day4/Stack1.java
+++ HRDRoadmap/HRDvtp7/day4/Stack1.java
@@ -3,12 +3,12 @@ public class Stack1 {
    static int [] arr;
    static int top=0;
    
-   public static int add(int [] arr, int n, int top){
+   public static int push(int [] arr, int n, int top){
         System.out.println(n+" added");
         arr[top++]=n;
         return top;
    }
-   public static int remove(int [] arr, int top){
+   public static int pop(int [] arr, int top){
         System.out.println(arr[top-1]+" removed");
         top--;
         return top;
@@ -19,18 +19,22 @@ public class Stack1 {
    public static int size(){
         return top;
    }
+    public static boolean isEmpty(){
+        return (top == -1);
+    }
     public static void main(String[] args) {
         int [] nums = new int[10];
 
         //Add
-        top = add(nums, 5, top);
-        top = add(nums, 52, top);
-        top = add(nums, 25, top);
+        top = push(nums, 5, top);
+        top = push(nums, 52, top);
+        top = push(nums, 25, top);
         System.out.print("stack: ");
         for(int i=0;i<top;i++){System.out.print(nums[i]+" ");}
+        System.out.println();
 
         //Remove
-        top = remove(nums, top);
+        top = pop(nums, top);
         System.out.print("stack: ");
         for(int i=0;i<top;i++){System.out.print(nums[i]+" ");}
         System.out.println();
@@ -40,6 +44,9 @@ public class Stack1 {
 
         //size
         System.out.println("Size: "+size());
+        
+        //is Empty
+        System.out.println("is empty? "+isEmpty());
         System.out.println();
     }
 }
