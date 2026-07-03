73ec6a3ac7046bc8571a2c39f2900a444bd5833d
shivam_raj <shivamraj81020051@gmail.com>
2026-07-03 14:16:26 +0530

    leetcode: 206

--- HRDRoadmap/HRDvtp7/day5/LC206.java
+++ HRDRoadmap/HRDvtp7/day5/LC206.java
@@ -1,12 +1,42 @@
 // package HRDRoadmap.HRDvtp7.day5;
-import java.lang.classfile.components.ClassPrinter.ListNode;
-import java.util.*;
 public class LC206 {
-    public static ListNode reverseList(ListNode head) {
-        
+    Node head;
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data,Node next){
+            this.data = data;
+            this.next = null;
+        }
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node reverseList(Node head) {
+        Node curr = head;
+        Node pre = null;
+        Node next;
+        while(curr != null){    // 1 -> 2 -> 3
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }return pre;
     }
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = null;
+
+        Node res = reverseList(n1);
+
+        while (res != null) {
+            System.out.println(res.data);
+            res=res.next;
+        }
     }
 }
