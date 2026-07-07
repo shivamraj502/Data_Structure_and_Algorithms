00045851fce7849b8dfb5ca8ff319d79a0194db0
shivam_raj <shivamraj81020051@gmail.com>
2026-07-07 11:31:33 +0530

    LC: 42

---
+++ HRDRoadmap/HRDvtp7/day8/LC40.java
@@ -0,0 +1,22 @@
+public class LC40 {
+    public static int trap(int[] height) {
+        int count = 0;
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        int [] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height));
+    }
+}
+
+/**
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ */
