108eec5b9d584557eea8aba1fa0e9b934821875c
shivam_raj <shivamraj81020051@gmail.com>
2026-07-07 11:32:16 +0530

    LC: 56

--- HRDRoadmap/HRDvtp7/day8/LC56.java
+++ HRDRoadmap/HRDvtp7/day8/LC56.java
@@ -1,7 +1,7 @@
 public class LC56 {
     public static  int[][] merge(int[][] intervals) {
         
-        
+        return new int[0][0];
     }
     public static void main(String[] args) {
         
