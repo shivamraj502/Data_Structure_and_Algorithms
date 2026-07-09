08bf2e6244cf1a4e6f21052f3585ecd392495916
shivam_raj <shivamraj81020051@gmail.com>
2026-07-09 14:18:17 +0530

    LC: 875

--- day50/LeetCode875.java
+++ day50/LeetCode875.java
@@ -31,36 +31,71 @@ public class LeetCode875 {
             if(sum==h){return i;}
         }return -1;   
     }
-    public static int minEatingSpeed2(int[] piles, int h) {
+    public static int minEatingSpeed2(long[] piles, long h) {
 
-        int min=piles[0];
-        int max=piles[0];
+        long min=piles[0];
+        long max=piles[0];
         for(int i=0;i<piles.length;i++){
             if(piles[i]<min){min=piles[i];}
             if(piles[i]>max){max=piles[i];}
         }//System.out.println("min: "+min+" ,max: "+max);
 
-        if(h<min){ min = h;}
+        if(h>max && piles.length==1){ return 1;}
         for(int i=1;i<=max;i++){ 
-            int sum=0;
+            long sum=0;
             for(int j=0;j<piles.length;j++){  // { 30,11,23,4,20 } div: 2 {15, 5.5, 11.5, 2, 10 }
-            sum += Math.ceilDiv(piles[j], i);
+                sum += Math.ceilDiv(piles[j], i);
             // System.out.println("sum: "+sum);
             }
-            if(sum==h){return i;}
+            if(sum<=h){return i;}
         }return -1;   
+    }
+    public static int minEatingSpeed3(long[] piles, long h) {
+
+    long max = piles[0];
+
+    for(int i = 0; i < piles.length; i++){
+        if(piles[i] > max){
+            max = piles[i];
+        }
+    }
+
+    long low = 1;
+    long high = max;
+    long ans = max;
+
+    while(low <= high){
+
+        long mid = low + (high - low) / 2;
+
+        long sum = 0;
+
+        for(int i = 0; i < piles.length; i++){
+            sum += Math.ceilDiv(piles[i], mid);
         }
 
+        if(sum <= h){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    return (int)ans;
+    }
+
     public static void main(String[] args) {
-        int[] piles = { 30,11,23,4,20 }; //speed = 50 banana / hour    { 30,11,23,4,20 }
-        int h = 5;
+        // int[] piles = { 30,11,23,4,20 }; //speed = 50 banana / hour    { 30,11,23,4,20 }
+        // int h = 5;
         // int h = 6;
-        // int[] piles = { 3,6,7,11 };  
-        // int h = 8;                                            
-        // int[] piles = {312884470 };    
-        // int h = 312884469;                                           
-        System.out.println("speed: "+minEatingSpeed2(piles, h));
-        // System.out.println(Math.ceil(15.2));
+        long[] piles = { 3,6,7,11 };  
+        long h = 8;                                            
+        // long[] piles = {312884470 };    
+        // long h = 312884469;                                           
+        System.out.println("speed: "+minEatingSpeed3(piles, h));
+        // System.out.println(Math.ceilDiv(312884470,312884469));
     }
 }
 
@@ -79,4 +114,9 @@ public class LeetCode875 {
  * Input: piles = [312884470], h = 312884469
  * Output: 23
  * Math.ceil(15,2)
+ */
+
+/**
+ * if(h>piles[j]) {sum +=1;}
+ * 
  */
