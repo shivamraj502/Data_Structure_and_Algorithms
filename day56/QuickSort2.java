82a8d63acfbf64ed4f4149be5d432c0729a06e54
shivam_raj <shivamraj81020051@gmail.com>
2026-07-11 23:58:19 +0530

    quick sort structure

--- day56/QuickSort2.java
+++ day56/QuickSort2.java
@@ -5,5 +5,11 @@ Problem: Quick Sort – GFG
 Goal: Learn partitioning and in-place swapping.
  */
 public class QuickSort2 {
-    
+    public static void QuickSort(int [] arr){
+
+    }
+    public static void main(String[] args) {
+        int [] arr = {5,8,9,88,4};
+        QuickSort(arr);
+    }
 }
