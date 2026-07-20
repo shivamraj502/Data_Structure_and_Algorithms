00def6ceb228261bbe6ff133378882066ade35c2
shivam_raj <shivamraj81020051@gmail.com>
2026-07-20 15:09:55 +0530

    LC: 509

--- day64/leetcode509.java
+++ day64/leetcode509.java
@@ -42,8 +42,31 @@ public class leetcode509 {
         return fib(n-1)+fib(n-2);
 
     }
+    public static int leetcode509b(int n){// 0 1 1 2 3 5 8 13 ...
+        if(n < 0){ System.out.println("invalid");return -1;}
+        if(n == 0 || n ==1) return n;
+
+        return leetcode509b(n-2)+leetcode509b(n-1);
+    }
     public static void main(String[] args) {
-        int n=6;
-        System.out.println("output: "+leetcode509(n));
+        int n=4;
+        System.out.println("output: "+leetcode509b(n));
     }
 }
+/**
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ */
