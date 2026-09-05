bbb4ac755bc48ad24d1318e6d8b57df98f65d3fb
shivam_raj <shivamraj81020051@gmail.com>
2026-09-06 01:06:41 +0530

    task remaining

---
+++ day88/LC.java
@@ -0,0 +1,27 @@
+/**
+Day 88 – Revision & Concept Integration
+Revise: Recursion flow, backtracking pruning, hashmap patterns.
+Re-solve 2 problems from each section.
+*/
+
+//package day88;
+public class LC {
+    public static void main(String[] args) {
+        System.out.println(6);
+    }
+}
+
+/**
+Day 88 – Revision Plan: Question List
+Section 1: Recursion Flow:
+Letter Combinations of a Phone Number – LC17
+Generate Parentheses – LC22
+
+Section 2: Backtracking Pruning
+Combination Sum II – LC40
+N-Queens – LC51
+
+Section 3: HashMap Patterns
+Top K Frequent Elements – LC347
+Group Anagrams – LC49
+ */
