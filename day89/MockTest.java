e99e1130dfc250329c954976d2d141bcf38e7694
shivam_raj <shivamraj81020051@gmail.com>
2026-09-06 23:46:50 +0530

    Mock test

---
+++ day89/MockTest.java
@@ -0,0 +1,11 @@
+/**
+Day 89 – Mock Test (DSA Round 1 Style)
+Solve 5 problems (1 recursion, 1 backtracking, 3 hashing).
+Target: 90 min limit.
+ */
+
+public class MockTest{
+    public static void main(String[] args) {
+        System.out.println(6);
+    }
+}
