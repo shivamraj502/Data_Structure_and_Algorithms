aa49c887d67c2052269f55d45f11120d60e7f7e6
shivam_raj <shivamraj81020051@gmail.com>
2026-09-14 21:17:56 +0530

    LC 1091

---
+++ day116/LC1091.java
@@ -0,0 +1,13 @@
+/**
+Day 116 – Shortest Path (BFS & Dijkstra)
+Problems:
+Shortest Path in Binary Matrix – LeetCode 1091
+Dijkstra Algorithm – GFG
+Goal: Master weighted shortest path algorithms.
+ */
+
+public class LC1091{
+    public static void main(String [] args){
+        System.out.println(5);
+    }
+}
