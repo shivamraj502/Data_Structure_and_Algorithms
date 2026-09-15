e44bb3511d12fcda07e12fba7c3bfe0a80f060de
shivam_raj <shivamraj81020051@gmail.com>
2026-09-15 22:20:59 +0530

    LC 261

---
+++ day117/LeetCode261.java
@@ -0,0 +1,11 @@
+/**
+Day 117 – Union-Find / Disjoint Set
+Problem: Graph Valid Tree – LeetCode 261
+Goal: Learn efficient component detection.
+ */
+
+public class LeetCode261 {
+    public static void main(String[] args) {
+        System.out.println(5);
+    }
+}
