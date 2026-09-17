5cf9d8ccf0da39b39c292e9937e3ed36f2d88fa0
shivam_raj <shivamraj81020051@gmail.com>
2026-09-17 14:40:43 +0530

    mock test

---
+++ day120/Test.java
@@ -0,0 +1,10 @@
+/**
+Day 120 – Revision + Mini Test
+Revise all topics (Tree, BST, Heap, Graph).
+Take a 90-min timed mock: 1 from each structure.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(55);
+    }
+}
